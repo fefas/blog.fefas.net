ba79aa2352b1688b050e5436829467808256c937
Felipe Martins <me@fefas.net>
2016-05-11 15:08:23 -0300

    Show name when email os clicked

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+var myName = 'Felipe Martins';
+
 $(document).ready(function () {
 
    $('#footer .name').click(function () {
-      $(this).html('em'.split("").reverse().join("") + '@' + window.location.hostname);
+      if ($(this).text() == myName) {
+         $(this).html('em'.split("").reverse().join("") + '@' + window.location.hostname);
+      } else {
+         $(this).html(myName);
+      }
    });
 });
