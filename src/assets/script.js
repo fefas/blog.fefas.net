0765987ee52b23ed9388b92234270001ccf3351d
Felipe Martins <6557756+fefas@users.noreply.github.com>
2021-01-06 19:52:42 -0300

    Define initial dark mode by system config (#79)
    * Implement dark theme activated by clicking d
    
    * Define dark mode by system config

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+function toggleDarkMode(doToggle) {
+    var bodyClasses = document.body.classList;
+    var darkThemeClass = 'dark';
+
+    doToggle ? bodyClasses.toggle(darkThemeClass) : false;
+=======
 function toggleDarkTheme() {
     var bodyClasses = document.body.classList;
     var darkThemeClass = 'dark';
@@ -5,9 +12,23 @@ function toggleDarkTheme() {
     bodyClasses.contains(darkThemeClass)
         ? bodyClasses.remove(darkThemeClass)
         : bodyClasses.add(darkThemeClass);
+>>>>>>> origin/main
 }
 
 document.addEventListener("keyup", function(event) {
     var dKeyCode = 68;
+<<<<<<< HEAD
+    toggleDarkMode(event.keyCode === dKeyCode);
+});
+
+window.onload = function() {
+    var darkModeEnabled =
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+     toggleDarkMode(darkModeEnabled);
+}
+=======
     event.keyCode === dKeyCode ? toggleDarkTheme() : false;
 });
+>>>>>>> origin/main
